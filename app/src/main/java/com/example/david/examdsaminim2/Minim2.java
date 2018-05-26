b53b2c544cc476364ba8e82897ab954ce303988d
Barbero95 <barberodavid95@yahoo.es>
2018-05-26 17:17:17 +0200

    primer commit

---
+++ app/src/main/java/com/example/david/examdsaminim2/Minim2.java
@@ -0,0 +1,18 @@
+package com.example.david.examdsaminim2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Minim2 extends AppCompatActivity {
+    //Main activity
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_minim2);
+
+
+
+
+
+    }
+}
